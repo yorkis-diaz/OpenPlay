164fb6378557b79c6b81e21d2767d392fd6a26fb
Yorkis Diaz <yorkis.i.diaz@gmail.com>
2019-12-12 10:17:27 -0500

    finished search bar styling

--- frontend/components/search_bar/search_bar.jsx
+++ frontend/components/search_bar/search_bar.jsx
@@ -10,15 +10,35 @@ class SearchBar extends React.Component {
 
 
     render () {
+        const { date } = this.state
+        const today = `${date.getFullYear()}-${date.getMonth() +
+          1}-${date.getDate()}`;
+
         return (
-            <div className="search-form">
-                <h1>Find your event for any occasion</h1>
-                {/* <form>
-                    <input type="date" min={}/>
-                </form> */}
+          <div className="search-form">
+            <h1>Find your event for any occasion</h1>
+            <div className="outer-div">
+              <div className="form-selects">
+                <input type="date" min={today} defaultValue={today} />
+                <span className="select-containers">
+                  <select>
+                    <option>1:00 PM</option>
+                  </select>
+                </span>
+                <select name="" id="">
+                  <option>1 Person</option>
+                </select>
+              </div>
+              <input
+                type="text"
+                name=""
+                placeholder={`${date.getFullYear()}/${date.getMonth() +
+                  1}/${date.getDate()}`}
+              />
+              <button>Let's go</button>
             </div>
-            
-        )
+          </div>
+        );
     }
 }
 
