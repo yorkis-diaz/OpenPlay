f489162e45ea6fd3f9786c70f86d9769117c0c4b
Yorkis Diaz <yorkis.i.diaz@gmail.com>
2019-12-16 10:43:25 -0500

    deleted saved_events regular controller

--- app/controllers/api/saved_events_controller.rb
+++ app/controllers/api/saved_events_controller.rb
@@ -1,2 +1,19 @@
 class Api::SavedEventsController < ApplicationController
+    def create
+        user = User.find(params[:id])
+        saved_event = user.saved_events.new(favorite_params)
+        if saved_event.saved
+            render json: { message: "Event saved successfully"}
+        else
+            render json: saved_event.errors.full_messages, status: 422
+        end
+    end
+
+    def destroy
+        
+    end
+
+    def favorite_params
+        params.require(:event).permit(:event_id)
+    end
 end
