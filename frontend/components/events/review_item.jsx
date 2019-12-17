539c8944df3190433e3f3791c09cdedf8961dfbd
Yorkis Diaz <yorkis.i.diaz@gmail.com>
2019-12-17 00:38:03 -0500

    removed debuggers

--- frontend/components/events/review_item.jsx
+++ frontend/components/events/review_item.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const ReviewItem = ({ review, reviewer, event }) => {
-  debugger
   return (
     <li key={review.id} className="review-item">
       <div className="reviewer-info">
