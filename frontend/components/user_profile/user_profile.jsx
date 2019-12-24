65ee5b208352c364d5f1963bc7e0bcfe335b971d
Yorkis Diaz <yorkis.i.diaz@gmail.com>
2019-12-24 00:23:35 -0500

    fixes to profile page

--- frontend/components/user_profile/user_profile.jsx
+++ frontend/components/user_profile/user_profile.jsx
@@ -6,68 +6,71 @@ import ProfileReservationItem from "./profile_reservation_item";
 class UserProfile extends React.Component {
   constructor(props) {
     super(props);
-  }
-
-  componentDidUpdate(prev) {
-
-  }
-
-
-
-
-    render() {
-      const { currentUser, reservations, savedEvents, events, deleteReservation } = this.props
-      const upcomingRes = []
-      const pastRes = [] 
-        reservations.map((reservation) => {
-            let reservationDate = new Date(reservation.date)
-            if (reservationDate <= new Date()) {
-                pastRes.push(
-                  <ProfileReservationItem
-                    key={reservation.id}
-                    reservation={reservation}
-                    event={events[reservation.event_id]}
-                    completed={true}
-                    deleteReservation={deleteReservation}
-                  />
-                );
-            } else {
-                upcomingRes.push(
-                  <ProfileReservationItem
-                    key = { reservation.id }
-                    reservation={reservation}
-                    event={events[reservation.event_id]}
-                    completed={false}
-                    deleteReservation={deleteReservation}
-                  />
-                );
-            }
-        })
-        return (
-            <>
-                <div className="name-container">
-                    <h1 className="user-profile-name">{currentUser.firstname} {currentUser.lastname}</h1>
-                </div>
-                <section className="profile-container">
-                    <div className="navigation-menu">
-                        <Link to="/user/profile">Reservations</Link> 
-                        <Link to="/user/saved-events">Saved Events</Link>
-                        {/* <button>Account Detail</button> */}
-                    </div>
-                    <main className="profile-content">
-                        <ul className="upcoming-res-ul">
-                            <h1 >Upcoming Reservations</h1>
-                            {upcomingRes}
-                        </ul>
-                        <ul className="past-res-ul">
-                            <h1>Past Reservations</h1>
-                            {pastRes}
-                        </ul>
-                    </main>
-                </section>
-            </>
-        )
+    this.state = {
+      date: new Date
     }
+  }
+
+  componentDidMount() {
+    this.setState({
+      date: new Date
+    })
+  }
+
+  render() {
+    const { currentUser, reservations, events, deleteReservation } = this.props
+    const upcomingRes = []
+    const pastRes = [] 
+    debugger
+    reservations.map((reservation) => {
+      const reservationDate = new Date(reservation.date).toDateString()
+      if (reservationDate >= new Date().toDateString()) {
+        pastRes.push(
+          <ProfileReservationItem
+            key={reservation.id}
+            reservation={reservation}
+            event={events[reservation.event_id]}
+            completed={true}
+            deleteReservation={deleteReservation}
+          />
+        );
+    } else {
+        upcomingRes.push(
+          <ProfileReservationItem
+            key = { reservation.id }
+            reservation={reservation}
+            event={events[reservation.event_id]}
+            completed={false}
+            deleteReservation={deleteReservation}
+          />
+        );
+      }
+    })
+    return (
+      <>
+        <div className="name-container">
+          <h1 className="user-profile-name">{currentUser.firstname} {currentUser.lastname}</h1>
+        </div>
+        <section className="profile-container">
+          <div className="navigation-menu">
+            <Link to="/user/profile">Reservations</Link> 
+            <Link to="/user/saved-events">Saved Events</Link>
+            {/* <button>Account Detail</button> */}
+          </div>
+          <main className="profile-content">
+            <ul className="upcoming-res-ul">
+              <h1 >Upcoming Reservations</h1>
+              {upcomingRes}
+            </ul>
+            <ul className="past-res-ul">
+              <h1>Past Reservations</h1>
+              {pastRes}
+            </ul>
+          </main>
+        </section>
+      </>
+    )
+  }
 }
 
 
