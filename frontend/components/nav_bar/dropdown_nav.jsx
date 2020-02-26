d825bb7bfa6ef0700fc59205cf48404d0d39fce8
Yorkis Diaz <yorkis.i.diaz@gmail.com>
2020-02-26 01:14:57 -0500

    remove debugger

--- frontend/components/nav_bar/dropdown_nav.jsx
+++ frontend/components/nav_bar/dropdown_nav.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const DropdownNav = ({ logout, closeDropdown, loggedIn, openModal, closeModal }) => {
-  debugger
     if (loggedIn) {
         return (
           <>
